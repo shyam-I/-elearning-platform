0bf9b42b5107c18b8b40745e81e277de589da33d
shyam-I <shyamhari2204@gmail.com>
2026-06-04 15:16:01 +0530

    Configured production CORS

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://elearning-platform-laqf.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 const db = mysql.createConnection({
